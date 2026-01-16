c65ea83c5b545662b36ab315cee15ad3343b7f16
Ali Shakeel <alishakeel567567@gmail.com>
2026-01-16 22:49:09 +0500

    init

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 import { Breadcrumb } from 'antd';
+import Breadpath from '../../assets/icons/braedpath.png';
 const App = (e) => (
   <Breadcrumb
     items={[
       {
         href: '',
         title: (<>
-          <HomeOutlined color='#0A5BE2' />
+        {/* <img src={Breadpath} alt="" sizes='12'/> */}
           <span className='text-[#0A5BE2]'>Dashboard</span>
         </>
         ),
