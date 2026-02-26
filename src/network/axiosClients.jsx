05e97c451ffc7b85504e15b4294e623f1a9854b7
Yaman Aamir <76522589+Abdullah-MotiWala@users.noreply.github.com>
2026-02-26 11:22:52 -0800

    Update axiosClients.jsx

--- src/network/axiosClients.jsx
+++ src/network/axiosClients.jsx
@@ -11,7 +11,7 @@ const Status = {
 };
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://stayhaven.pk/api/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
